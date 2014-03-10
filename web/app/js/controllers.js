39f2629dc649bcf18678d1d4b8938cd13fefe163
Dmitrijs Sadovskis <dmitrijs.sadovskis@gmail.com>
2014-03-10 23:53:39 +0200

    implemented DELETE /api/vhosts (to delete vhosts)

--- web/app/js/controllers.js
+++ web/app/js/controllers.js
@@ -4,12 +4,19 @@ angular.module('vmApp.controllers', []).
     controller('vmCtrl', function ($scope, $http) {
         $scope.formData = {};
         
-        $scope.add = function () {
+        $scope.addHost = function () {
           $http.post('/api/vhosts', $scope.formData)
               .success(function (data) {
                   $scope.formData = {};
                   $scope.vhosts = data;
               });
+        };
+        
+        $scope.deleteHost = function (nn) {
+            $http.delete('/api/vhosts/' + nn)
+                .success(function (data) {
+                    $scope.vhosts = data;
+                });
         };
         
         $http.get('/api/vhosts').success(function (data) {
--- web/index.php
+++ web/index.php
@@ -1,4 +1,5 @@
 <?php
+ini_set('date.timezone', 'Europe/Riga');
 
 require 'vendor/autoload.php';
 
@@ -11,11 +12,12 @@ function get_all_vhosts($filename) {
 
 	$all = array();
 	foreach (preg_split('/\r|\n|\r\n/', $file) as $line) {
-		if (preg_match('/^## Path: (.*?) ServerName: (.*?)$/', $line, $matches)) {
-			$v_path = $matches[1];
-			$v_server_name = $matches[2];
+		if (preg_match('/^## #(.*?) Path: (.*?) ServerName: (.*?)$/', $line, $matches)) {
+			$v_hash = $matches[1];
+			$v_path = $matches[2];
+			$v_server_name = $matches[3];
 
-			$all[] = array('path' => $v_path, 'server_name' => $v_server_name);
+			$all[] = array('nn' => $v_hash, 'path' => $v_path, 'server_name' => $v_server_name);
 		}
 	}
 	return $all;
@@ -27,17 +29,43 @@ $app->get('/api/vhosts', function () use ($filename) {
 
 $app->post('/api/vhosts', function () use ($app, $filename) {
 	$post = json_decode(file_get_contents('php://input'), true);
-	
+
 	if (isset($post['path']) && isset($post['server_name'])) {
 		$v_path = str_replace('\\', '/', $post['path']);
 		$v_server_name = $post['server_name'];
 
 		$template = file_get_contents('./config/vhost.template.conf');
+		$template = str_replace('{{NN}}', md5($v_server_name), $template);
 		$template = str_replace('{{PATH}}', $v_path, $template);
 		$template = str_replace('{{SERVER_NAME}}', $v_server_name, $template);
 
 		file_put_contents($filename, $template . "\n", FILE_APPEND);
 	}
+	die(json_encode(get_all_vhosts($filename)));
+});
+
+$app->delete('/api/vhosts/:nn', function ($nn) use ($app, $filename) {
+	$lines = file($filename, FILE_IGNORE_NEW_LINES);
+
+	$backup_filename = 'backup/' . $filename . '.' . date('Ymd_Hi');
+	$new_filename = $filename . '.updated';
+	file_put_contents($new_filename, '');
+
+	$do_print = false;
+	foreach ($lines as $line) {
+		if (preg_match('/^## #([0-9a-f]+) /', $line, $matches)) {
+			$do_print = ! ($matches[1] === $nn);
+		}
+		if ($do_print) {
+			file_put_contents($new_filename, $line . "\n", FILE_APPEND);
+		}
+		if (preg_match('/^## End/', $line)) {
+			$do_print = true;
+		}
+	}
+	rename($filename, $backup_filename);
+	rename($new_filename, $filename);
+
 	die(json_encode(get_all_vhosts($filename)));
 });
 
